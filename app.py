8c019797dae0f512a77a76cdc96ce74a1185ba43
Michael Vievard <michael.vievard@docaposte.fr>
2022-10-12 15:03:40 +0200

    update after add pods x4

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World by mika!"
+    return "Hello World by mika avec plus de pods!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
